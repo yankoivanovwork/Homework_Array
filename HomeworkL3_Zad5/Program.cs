ed00f154997ecffacf037928e0655d8a2d418c12
yankoivanovwork <51404578+yankoivanovwork@users.noreply.github.com>
2019-07-18 11:47:33 +0300

    Update Program.cs

--- HomeworkL3_Zad5/Program.cs
+++ HomeworkL3_Zad5/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 
 namespace HomeworkL3_Zad5
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             ///variant 0 - razlichna promenliva za vsqka proverka, masiv - pass
 
