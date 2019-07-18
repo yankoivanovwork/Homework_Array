a4a3875b6bb495043f3e6bd904bef2c5e5a1bef6
yankoivanovwork <51404578+yankoivanovwork@users.noreply.github.com>
2019-07-18 11:43:56 +0300

    Zad5_Fix

---
+++ HomeworkL3_Zad5/Condition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkL3_Zad5
+{
+    public enum Condition
+    {
+        isMoreThanSixDigits = 1,
+        containsADigit = 2,
+        containsLowercase = 3,
+        containsUppercase = 4,
+        containsSpecialCharacter = 5
+    }
+}
--- HomeworkL3_Zad5/Program.cs
+++ HomeworkL3_Zad5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace HomeworkL3_Zad5
@@ -7,52 +8,121 @@ namespace HomeworkL3_Zad5
     {
         static void Main(string[] args)
         {
-            char[] password = Console.ReadLine().ToCharArray();
-            bool[] condition = new bool[5];
+            ///variant 0 - razlichna promenliva za vsqka proverka, masiv - pass
+
+            ///variant 1 - dictionary
+            Dictionary<Condition, bool> strongPasswordConditionDictionary = new Dictionary<Condition, bool>()
+            {
+                { Condition.isMoreThanSixDigits, false },
+                { Condition.containsADigit, false },
+                { Condition.containsLowercase, false },
+                { Condition.containsUppercase, false },
+                { Condition.containsSpecialCharacter, false }
+            };
+            ///------end dictionary
+
+            ///variant 2 named tuple
+            (bool aboveSixCharacters, bool containsNumber, bool containsLowercase, bool containsUppercase, bool containsSpecialCharacter) strongPasswordConditionTuple = (false, false, false, false, false);
+            //var strongPasswordCondition = (aboveSixCharacters: false, containsNumber: false, containsLowercase: false, containsUppercase : false, containsSpecialCharacter : false);
+            //strongPasswordConditionTuple.aboveSixCharacters = true;
+            ///-----end named tuple
+
+            Console.WriteLine("Въведете парола за тестване: ");
+            string password = Console.ReadLine();
+
+            strongPasswordConditionDictionary[Condition.isMoreThanSixDigits] = ValidatePasswordLength(password);
 
             for (int i = 0; i < password.Length; i++)
             {
-                if (password.Length >= 6)
+                if (strongPasswordConditionDictionary[Condition.containsADigit] == false)
                 {
-                    condition[0] = true;
+                    strongPasswordConditionDictionary[Condition.containsADigit] = ValidatePasswordHasAleastOneDigit(password[i]);
                 }
-                if (char.IsDigit(password.ElementAtOrDefault(i)))
+
+                if (strongPasswordConditionDictionary[Condition.containsLowercase] == false)
                 {
-                    condition[1] = true;
+                    strongPasswordConditionDictionary[Condition.containsLowercase] = ValidatePasswordHasLowerCaseChar(password[i]);
                 }
-                if (char.IsLower(password.ElementAtOrDefault(i)))
+
+                if (strongPasswordConditionDictionary[Condition.containsUppercase] == false)
                 {
-                    condition[2] = true;
+                    strongPasswordConditionDictionary[Condition.containsUppercase] = ValidatePasswordHasUpperCaseChar(password[i]);
                 }
-                if (char.IsUpper(password.ElementAtOrDefault(i)))
+
+                if (strongPasswordConditionDictionary[Condition.containsSpecialCharacter] == false)
                 {
-                    condition[3] = true;
-                }
-                if (char.IsSymbol(password.ElementAtOrDefault(i)) || char.IsPunctuation(password.ElementAtOrDefault(i)))
-                {
-                    condition[4] = true;
+                    strongPasswordConditionDictionary[Condition.containsSpecialCharacter] = ValidatePasswordHasSpecialChar(password[i]);
                 }
             }
 
-            if (condition.All(x => x == true))
+            Console.WriteLine(DisplayResult(strongPasswordConditionDictionary));
+        }
+
+        //Console.WriteLine("Contains Digit? .Any(): " + password.Any(char.IsDigit));
+
+        private static bool ValidatePasswordLength(string password)
+        {
+            return password.Length >= 6 ? true : false;
+        }
+
+        private static bool ValidatePasswordHasAleastOneDigit(char password)
+        {
+            return char.IsDigit(password) ? true : false;
+        }
+
+        private static bool ValidatePasswordHasLowerCaseChar(char password)
+        {
+            return char.IsLower(password) ? true : false;
+        }
+
+        private static bool ValidatePasswordHasUpperCaseChar(char password)
+        {
+            return char.IsUpper(password) ? true : false;
+        }
+
+        private static bool ValidatePasswordHasSpecialChar(char password)
+        {
+            return (char.IsPunctuation(password) || char.IsSeparator(password)) ? true : false;
+        }
+
+        private static string DisplayResult(Dictionary<Condition, bool> strongPasswordConditionDictionary)
+        {
+            string displayString = string.Empty;
+            bool isPasswordStrong = true;
+
+            for (int i = 0; i < strongPasswordConditionDictionary.Count; i++)
             {
-                Console.WriteLine("Силна парола!");
+                if (!strongPasswordConditionDictionary.Values.ElementAtOrDefault(i))
+                {
+                    isPasswordStrong = false;
+                    break;
+                }
             }
-            else if (!condition.Contains(true))
+
+            if (isPasswordStrong)
             {
-                Console.WriteLine("False, паролата не отговаря на нито едно от посочените условия!");
+                displayString = "Password is strong!";
             }
             else
             {
-                Console.Write("False, Паролата не изпълнява следните условия:");
-                Console.Write(condition.FirstOrDefault() == false ? " да съдържа минимум от 6 символа," : string.Empty);
-                Console.Write(condition.ElementAtOrDefault(1) == false ? " поне една цифра," : string.Empty);
-                Console.Write(condition.ElementAtOrDefault(2) == false ? " поне една малка буква," : string.Empty);
-                Console.Write(condition.ElementAtOrDefault(3) == false ? " поне една голяма буква," : string.Empty);
-                Console.Write(condition.ElementAtOrDefault(4) == false ? " поне един символ различен от цифра и буква" : string.Empty);
-                Console.Write(".");
-                Console.Write(Environment.NewLine);
+                displayString = "False, Паролата не изпълнява следните условия: ";
+                if (!strongPasswordConditionDictionary[Condition.isMoreThanSixDigits])
+                    displayString += " да съдържа минимум от 6 символа,";
+
+                if (!strongPasswordConditionDictionary[Condition.containsADigit])
+                    displayString += " поне една цифра,";
+
+                if (!strongPasswordConditionDictionary[Condition.containsLowercase])
+                    displayString += " поне една малка буква,";
+
+                if (!strongPasswordConditionDictionary[Condition.containsUppercase])
+                    displayString += " поне една голяма буква,";
+
+                if (!strongPasswordConditionDictionary[Condition.containsSpecialCharacter])
+                    displayString += " поне един символ различен от цифра и буква";
             }
+
+            return displayString;
         }
     }
 }
